bab0db1dc25a4dd61bc101ec69e7ef0b9863f413
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2022-05-30 16:39:26 +0200

    add: prettier on safe

--- src/integrals.jl
+++ src/integrals.jl
@@ -1,12 +1,12 @@
 circleintegral(f, x₀::Real, r::Real) =
-    quadgk(ϕ->f(x₀+r*cis(ϕ))*r*cis(ϕ), -7π/8, 9π/8)[1]/(2π) # cancel 1im
+    quadgk(ϕ -> f(x₀ + r * cis(ϕ)) * r * cis(ϕ), -7π / 8, 9π / 8)[1] / (2π) # cancel 1im
 # 
 circleintegral(f, x₀::Real, r::Real, N::Int) =
-    sum(f(x₀+r*cis(ϕ))*r*cis(ϕ) for ϕ in range(-7π/8, 9π/8, length=N+1)[2:end])[1]/N # 2π i cancels
+    sum(f(x₀ + r * cis(ϕ)) * r * cis(ϕ) for ϕ in range(-7π / 8, 9π / 8, length=N + 1)[2:end])[1] / N # 2π i cancels
 #
-cauchyintegral(  F, x₀::Real, r) = circleintegral(x′-> F(x′+x₀)/x′,   0.0, r)
-cauchyintegral′( F, x₀::Real, r) = circleintegral(x′-> F(x′+x₀)/x′^2, 0.0, r)
-cauchyintegral′′(F, x₀::Real, r) = circleintegral(x′->2F(x′+x₀)/x′^3, 0.0, r)
+cauchyintegral(F, x₀::Real, r) = circleintegral(x′ -> F(x′ + x₀) / x′, 0.0, r)
+cauchyintegral′(F, x₀::Real, r) = circleintegral(x′ -> F(x′ + x₀) / x′^2, 0.0, r)
+cauchyintegral′′(F, x₀::Real, r) = circleintegral(x′ -> 2F(x′ + x₀) / x′^3, 0.0, r)
 # 
 
 
@@ -20,7 +20,7 @@ struct CircularSum <: CircularIntegralMethod
 end
 
 circleintegral(F, x₀::Real, method::CircularIntegral) = circleintegral(F, x₀, method.r)
-circleintegral(F, x₀::Real, method::CircularSum) = circleintegral(F, x₀,method.r, method.N)
+circleintegral(F, x₀::Real, method::CircularSum) = circleintegral(F, x₀, method.r, method.N)
 
 
 
@@ -30,7 +30,7 @@ struct ComplexBranchPointExpansion <: EffectiveRangeExpansionMethod
 end
 
 effectiverangeexpansion(f, k, r::Float64) =
-effectiverangeexpansion(f, k, ComplexBranchPointExpansion(CircularIntegral(r)))
+    effectiverangeexpansion(f, k, ComplexBranchPointExpansion(CircularIntegral(r)))
 
 """
 f(s) / N = a⁻¹ + r k(s)^2 / 2 - i k(s)
@@ -38,17 +38,17 @@ f(s) / N = a⁻¹ + r k(s)^2 / 2 - i k(s)
 Is calculated using cauchy integral theorem.
 """
 function effectiverangeexpansion(f, k, method::EffectiveRangeExpansionMethod)
-    N = circleintegral(f,0.0,method.cim) /
-        circleintegral(k,0.0,method.cim) / (-1im)
+    N = circleintegral(f, 0.0, method.cim) /
+        circleintegral(k, 0.0, method.cim) / (-1im)
     # 
-    f̂₀  = cauchyintegral(  x->f(x)/N + 1im*k(x), 0.0, method.cim)
-    f̂′  = cauchyintegral′( x->f(x)/N + 1im*k(x), 0.0, method.cim)
+    f̂₀ = cauchyintegral(x -> f(x) / N + 1im * k(x), 0.0, method.cim)
+    f̂′ = cauchyintegral′(x -> f(x) / N + 1im * k(x), 0.0, method.cim)
     # 
     # k²₀  = cauchyintegral(  x->k(x)^2, 0.0, r)
-    k²′  = cauchyintegral′( x->k(x)^2, 0.0, method.cim)
+    k²′ = cauchyintegral′(x -> k(x)^2, 0.0, method.cim)
     # 
     a⁻¹ = f̂₀
-    r = 2 * f̂′/k²′
+    r = 2 * f̂′ / k²′
     #
     return (; a⁻¹, r, N)
 end
